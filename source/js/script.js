68e3a957973045a408a18486259f2845ef127826
Sergei Kolesnikov <driveog187@gmail.com>
2023-01-22 13:14:52 +0300

    скрывает бургерное меню при отключенном js

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,18 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-header__toggle');
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-header__toggle");
+let mainHeader = document.querySelector(".main-header__box");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
+mainHeader.classList.remove("main-header__box--nojs");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.classList.add('main-header__toggle--switch');
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    navToggle.classList.add("main-header__toggle--switch");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.classList.remove('main-header__toggle--switch');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    navToggle.classList.remove("main-header__toggle--switch");
   }
 });
